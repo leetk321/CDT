5a66a1410c5448fc9107a29f6cfcd66f1133a258
청포도 <leetk321@hanmail.net>
2019-05-08 14:42:01 +0900

    최초 버전

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>클랜 CDT 링크 모음</title>
+        <meta name="description" content="CDT 링크 모음">
+    </head>
+    <body>
+        <h1>클랜 CDT 링크 모음</h1>
+        <h2>귀찮으니까 디자인 천천히 할거임</h2>
+        <table>
+        <tr>
+            <td>클랜 시트</td>
+            <td><a href = "https://goo.gl/nurbLT">https://goo.gl/nurbLT</a></td>
+        </tr>
+        <tr>
+            <td>클랜 아카이브</td>
+            <td><a href = "https://goo.gl/vsQ8SG">https://goo.gl/vsQ8SG</a></td>
+        </tr>
+            <td>클랜 디스코드</td>
+            <td><a href = "https://discord.gg/5sTkKVP">https://discord.gg/5sTkKVP</a></td>
+        <tr>
+            <td>직장인 클랜연합</td>
+            <td><a href = "https://cafe.naver.com/owncw">https://cafe.naver.com/owncw</a></td>
+        </tr>
+        </table>
+        
+        <h2>팀 시트 링크</h2>
+        
+        <table>
+        <tr>
+            <td>팀 Vitamin</td>
+            <td><a href = "https://goo.gl/UqR35y">https://goo.gl/UqR35y</a></td>        
+        </tr>
+        <tr>
+            <td>팀 Academy</td>
+            <td><a href = "https://bit.ly/2Ye4CVD">https://bit.ly/2Ye4CVD</a></td>
+        </tr>
+        </table>
+    </body>
+</html>
